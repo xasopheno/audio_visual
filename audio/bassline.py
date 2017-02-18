60584efa604989bededf802f05c82e41d5da2337
Danny Meyer <danny.meyer@gmail.com>
2017-02-17 23:11:15 -0500

    Adds test frequencies

--- audio/bassline.py
+++ audio/bassline.py
@@ -2,40 +2,37 @@
 import math
 import numpy
 import pyaudio
-import matplotlib.pyplot as plt
 
 
 def sine(frequency, length, rate):
     length = int(length * rate)
     factor = (float(frequency) * (math.pi * 2) / rate)
-    waveform = numpy.sin(numpy.arange(length) * factor)
-    return waveform
+    return numpy.sin(numpy.arange(length) * factor)
 
 
 def play_tone(stream, frequency, length, rate=44100):
-    chunks = []
-    chunks.append(sine(frequency, length, rate))
+    chunks = [sine(frequency, length, rate)]
 
-    fade = 250.
+    chunk = numpy.concatenate(chunks) * 0.25
+
+    fade = 200.
 
     fade_in = numpy.arange(0., 1., 1/fade)
     fade_out = numpy.arange(1., 0., -1/fade)
 
-    chunk = numpy.concatenate(chunks) * 0.25
     chunk[:fade] = numpy.multiply(chunk[:fade], fade_in)
     chunk[-fade:] = numpy.multiply(chunk[-fade:], fade_out)
 
-    rampOut = chunk[-1000:]
-    print rampOut.shape
-
     stream.write(chunk.astype(numpy.float32).tostring())
 
-def doit():
-    frequency1 = 700
-    frequency2 = 250
+
+def test():
+    test_freqs = [50, 100, 200, 400, 800, 1200, 2000, 3200]
+
     for i in range(2):
-        play_tone(stream, frequency1, 2)
-        play_tone(stream, frequency2, 2)
+        for freq in test_freqs:
+            play_tone(stream, freq, 1)
+
 
 if __name__ == '__main__':
     p = pyaudio.PyAudio()
@@ -43,16 +40,4 @@ if __name__ == '__main__':
                     channels=1, rate=44100, output=1)
 
 
-doit()
-
-
-
-#     
-
-# import numpy as np
-# from scipy.io.wavfile import write
-
-# data = np.random.uniform(-100,100,444100) # 44100 random samples between -1 and 1
-# scaled = np.int16(data/np.max(np.abs(data)) * 32767)
-# write('test.wav', 44100, scaled)
-
+test()
