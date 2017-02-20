d3a78c99a43b49ef53335ae2a50134eecda70df9
Danny Meyer <danny.meyer@gmail.com>
2017-02-20 02:17:32 -0500

    Add's plotting

--- audio/oscillator.py
+++ audio/oscillator.py
@@ -1,6 +1,8 @@
 from __future__ import division
 from math import pi
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def wave(frequency, length, rate):
     """produces sine across np array"""
@@ -9,25 +11,25 @@ def wave(frequency, length, rate):
     factor = float(frequency) * (pi * 2) / rate
     waveform = np.sin(np.arange(length) * factor)
 
-    # waveform = np.round(waveform)
+    waveform = np.round(waveform)
 
     waveform2 = np.power(waveform, 3)
 
-    # return waveform2
+    # return waveform
     return np.add(waveform, waveform2)
+
 
 def play_frequencies(stream, length, volume, attack, decay, *freqs):
     """Plays a group of frequencies"""
 
-    allTones = []
+    all_tones = []
 
     for freq in freqs:
-        chunks = []
-        chunks.append(wave(freq, length, 44100))
-        chunk = np.concatenate(chunks) * volume
+        waveform = [wave(freq, length, 44100)]
+        chunk = np.concatenate(waveform) * volume
 
         attack = attack
-        decay = attack
+        decay = decay
 
         fade_in = np.arange(0., 1., 1./attack)
         fade_out = np.arange(1., 0., -1./decay)
@@ -35,11 +37,14 @@ def play_frequencies(stream, length, volume, attack, decay, *freqs):
         chunk[:attack] = np.multiply(chunk[:attack], fade_in)
         chunk[-decay:] = np.multiply(chunk[-decay:], fade_out)
 
-        allTones.append(chunk)
+        all_tones.append(chunk)
 
-    chunk = sum(allTones)
+    all_tones = sum(all_tones)
 
-    stream.write(chunk.astype(np.float32).tostring())
+    plt.plot(all_tones)
+    plt.show()
+
+    stream.write(all_tones.astype(np.float32).tostring())
 
 
 # propertysOfATone = [
