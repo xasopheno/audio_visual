e2f162cca5d5fce86ebe03b1b1b45d70e4cce862
Danny Meyer <danny.meyer@gmail.com>
2017-04-05 09:02:26 -0400

    WIp

--- audio/detect_freqs.py
+++ audio/detect_freqs.py
@@ -2,17 +2,15 @@ from __future__ import division
 import pyaudio
 import numpy
 import scipy.io.wavfile as wav
-
-from numpy import argmax, mean, diff, log
-
-from scipy.signal import blackmanharris, fftconvolve
-from matplotlib.mlab import find
+from numpy.fft import rfft
+from numpy import argmax, log
+from scipy.signal import kaiser
 import butter_bandpass_filter
 from parabolic import parabolic
 from Oscillators.sine_osc import SineOsc
 
 RATE = 44100
-RECORD_SECONDS = 15
+RECORD_SECONDS = 5
 CHUNKSIZE = 2048
 
 osc = SineOsc()
@@ -24,28 +22,27 @@ stream2 = p.open(format=pyaudio.paFloat32,
                  channels=1, rate=RATE, output=2)
 
 
-def get_cycle_length(sig, fs):
+def get_cycle_length(signal, fs):
+    """Estimate frequency from peak of FFT
+    Pros: Accurate, usually even more so than zero crossing counter
+    (1000.000004 Hz for 1000 Hz, for instance).  Due to parabolic
+    interpolation being a very good fit for windowed log FFT peaks?
+    https://ccrma.stanford.edu/~jos/sasp/Quadratic_Interpolation_Spectral_Peaks.html
+    Accuracy also increases with signal length
+    Cons: Doesn't find the right value if harmonics are stronger than
+    fundamental, which is common.
     """
-    Estimate frequency using autocorrelation
-    """
+    N = len(signal)
 
-    # Calculate autocorrelation (same thing as convolution, but with
-    # one input reversed in time), and throw away the negative lags
-    corr = fftconvolve(sig, sig[::-1], mode='full')
-    corr = corr[len(corr)//2:]
+    # Compute Fourier transform of windowed signal
+    windowed = signal * kaiser(N, 100)
+    f = rfft(windowed)
+    # Find the peak and interpolate to get a more accurate peak
+    i_peak = argmax(abs(f))  # Just use this value for less-accurate result
+    i_interp = parabolic(log(abs(f)), i_peak)[0]
 
-    # Find the first low point
-    d = diff(corr)
-    start = find(d > 0)[0]
-
-    # Find the next peak after the low point (other than 0 lag).  This bit is
-    # not reliable for long signals, due to the desired peak occurring between
-    # samples, and other peaks appearing higher.
-    # Should use a weighting function to de-emphasize the peaks at longer lags.
-    peak = argmax(corr[start:]) + start
-    px, py = parabolic(corr, peak)
-
-    return round(fs / px, 0)
+    # Convert to equivalent frequency
+    return round(fs * i_interp / N, 0)
 
 frequencies = []
 frames = []
@@ -65,8 +62,15 @@ for i in range(0, int(RATE / CHUNKSIZE * RECORD_SECONDS)):
 #
 # wav.write('out.wav', RATE, numpydata)
 #
+past_freq = 0
 for freq in frequencies:
-    osc.play_frequencies(stream2, .0249, 1, 100, 100, freq)
+    # if freq - freq
+    #     freq = past_freq
+    osc.play_frequencies(stream2, CHUNKSIZE/RATE, 1, 100, 100, freq,
+                         freq / 2,
+                         freq * 3/2,
+                         freq * 2,
+                         freq * 7/4)
     print freq
 
 
