ecec3c7982343b0de1c28799630097e0f0299a13
Danny Meyer <danny.meyer@gmail.com>
2017-04-12 09:11:18 -0700

    Generates file_names

--- audio/file_namer.py
+++ audio/file_namer.py
@@ -1,15 +1,8 @@
+from datetime import datetime
 # stream that's coming in at 44,100 sps
 # which is turned into 1024 sample chunks.
 #
 # OUTPUT_DIRECTORY
-#
-# def WriteFileName():
-#     What note is this?
-#     Ab
-# noteName = 'Ab'
-# noteNumber = String(note_to_number[noteName])
-# date = 04_04_17
-# output_filename = noteName + '_' + date + '_' + noteNumber
 
 note_to_number = {
     'F5': 32,
@@ -48,3 +41,18 @@ note_to_number = {
     'B2': 2,
     'Bb2': 1,
 }
+
+def file_namer():
+    note_name = raw_input('Which note is this?: ')
+    now = datetime.now()
+
+    for key in note_to_number:
+        if key == note_name:
+            note_number = note_to_number[key]
+
+    # G3__1__2017_04_12__08_58_18__10
+    for i in range(20):
+        file_name =  now.strftime(note_name + '__' + str(i) + '__%Y_%m_%d__%H_%M_%S__' + str(note_number))
+        print file_name
+
+file_namer()
