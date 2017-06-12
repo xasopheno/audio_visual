81ac0eb478f7ef109d08c4b71eb876de50dae6f7
Danny Meyer <danny.meyer@gmail.com>
2017-06-11 20:40:11 -0400

    Composer

--- audio/Composer/comp_osc.py
+++ audio/Composer/comp_osc.py
@@ -21,36 +21,30 @@ class SineOsc:
         waveform2 = np.power(waveform, 3)
         waveform3 = np.power(rounded_waveform, 4)/4
 
-        return np.add(waveform, np.add(waveform, waveform2))
+        return np.add(waveform, np.add(waveform2, waveform3))
 
-    def play_frequencies(self, length, volume, attack, decay, *freqs):
+    def play_frequencies(self, stream, length, volume, attack, decay, *freqs):
         """Plays a group of frequencies"""
-        volume *= .25
         all_tones = []
 
         for freq in freqs:
-            if freq > 1000:
-                volume = volume * .80902
-            chunks = [self.wave(freq, length, self.sample_rate)]
-            chunk = np.concatenate(chunks) * volume
-
-            attack = attack
-            decay = decay
+            wave = [self.wave(freq, length, self.sample_rate)]
+            waveform = np.concatenate(wave) * volume
 
             fade_in = np.arange(0., 1., 1./attack)
             fade_out = np.arange(1., 0., -1./decay)
 
-            chunk[:attack] = np.multiply(chunk[:attack], fade_in)
-            chunk[-decay:] = np.multiply(chunk[-decay:], fade_out)
+            waveform[:attack] = np.multiply(waveform[:attack], fade_in)
+            waveform[-decay:] = np.multiply(waveform[-decay:], fade_out)
 
-            all_tones.append(chunk)
+            all_tones.append(waveform)
 
         all_tones = sum(all_tones)
 
         # plt.plot(chunk[])
         # plt.show()
 
-        return all_tones
+        return stream.write(all_tones.astype(np.float32).tostring())
 
     def play_multiple_parts(self, stream, *parts):
         all_parts = []
--- audio/Composer/two_part.py
+++ audio/Composer/two_part.py
@@ -1,25 +1,83 @@
 import random
 import pyaudio
 from comp_osc import SineOsc
+import multiprocessing as mp
+import time
 
 sine_osc = SineOsc()
 
 p = pyaudio.PyAudio()
+p2 = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=44100,
                 output=1,
                 )
 
+stream2 = p2.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=44100,
+                output=1,
+                )
 
-one = [2, 1, 200, 200, 400]
-two = [2, .1, 200, 200, 840]
 
-three = [2, .1, 200, 200, 500]
-four = [2, .5, 200, 200, 900]
+def func1():
+    print ('func1')
+    for i in range(20):
+        sine_osc.play_frequencies(stream, .1, 1, 1000, 1000,
+                                  200, 300)
+        sine_osc.play_frequencies(stream, .1, 1, 1000, 1000,
+                                  240, 340)
+        sine_osc.play_frequencies(stream, .1, 1, 1000, 1000,
+                                  200, 300)
+        sine_osc.play_frequencies(stream, .2, 1, 1000, 1000,
+                                  240, 340)
+        sine_osc.play_frequencies(stream, .7, 1, 1000, 1000,
+                                  240, 340)
+        sine_osc.play_frequencies(stream, .3, 1, 1000, 1000,
+                                  260, 360)
+        sine_osc.play_frequencies(stream, .8, 1, 1000, 1000,
+                                  0)
 
-sine_osc.play_multiple_parts(stream, one, two)
-sine_osc.play_multiple_parts(stream, three, four)
+
+
+def func2():
+    print ('func2')
+    for i in range(10):
+        sine_osc.play_frequencies(stream, 1, 1.5, 1000, 30000,
+                                  100, 150, 50, 50)
+        sine_osc.play_frequencies(stream, 1, 1.5, 1000, 30000,
+                                  100, 150, 50, 50)
+        sine_osc.play_frequencies(stream, 1, 1.5, 1000, 30000,
+                                  100, 150, 50, 50)
+        sine_osc.play_frequencies(stream, 1, 1.5, 1000, 30000,
+                                  100 * 7/8, 150* 7/8, 50* 7/8, 50* 7/8)
+
+
+def func3():
+    print ('func3')
+    for i in range(5):
+        for i in range(20):
+            sine_osc.play_frequencies(stream, .1, 0, 1000, 2000,
+                                      0)
+            sine_osc.play_frequencies(stream, .2, .2, 5000, 5000,
+                                      1400)
+        time.sleep(4)
+
+
+
+if __name__=='__main__':
+    mp.set_start_method('spawn')
+    p1 = mp.Process(target=func1)
+    p1.start()
+    p2 = mp.Process(target=func2)
+    time.sleep(4)
+    p2.start()
+    time.sleep(4)
+    p3 = mp.Process(target=func3)
+    p3.start()
+
+
 
 # I will pass you multiple series of notes and you will prepare to play them.
 # When they are all ready, you will combine them and produce a single audio file.
