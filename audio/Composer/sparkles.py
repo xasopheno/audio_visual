9dbcc1edb4a53a2c5966409d8becc349f73cd95f
Danny Meyer <danny.meyer@gmail.com>
2017-07-12 20:51:15 -0400

    Updates sparkles

--- audio/Composer/sparkles.py
+++ audio/Composer/sparkles.py
@@ -21,14 +21,13 @@ stream2 = p2.open(format=pyaudio.paFloat32,
                   )
 
 
-
 def func1():
     print ('func1')
     freq = 10000
     for i in range(10000000000):
-        sine_osc.play_frequencies(stream, .05, random.choice([.90, 1, 1, 1, 1.05]) /3, 50, 50,
+        sine_osc.play_frequencies(stream, .05, random.choice([.90, 1, 1, 1, 1.05]) / 2, 50, 50,
                                   freq,
-                                  freq + 1,
+                                  freq / 1000,
                                   )
         freq += 50
 
@@ -37,23 +36,33 @@ def func2():
     print ('func2')
     freq = 10000
     for i in range(10000000000):
-        sine_osc.play_frequencies(stream, .07, random.choice([.90, 1, 1, 1, 1.05]) /3, 50, 50,
+        sine_osc.play_frequencies(stream, .07, random.choice([.90, 1, 1, 1, 1.05]) / 2, 50, 50,
                                   freq,
-                                  # freq + 2
+                                  freq / 100
                                   )
         freq += 25
+
 
 def func3():
     print ('func3')
     freq = 10000
     for i in range(10000000000):
-        sine_osc.play_frequencies(stream, .09, random.choice([.90, 1, 1, 1, 1.05]) / 3, 50, 50,
+        sine_osc.play_frequencies(stream, .09, random.choice([.90, 1, 1, 1, 1.05]) / 2, 50, 50,
                                   freq,
-                                  # freq + 3,
+                                  freq / 10000,
                                   )
         freq += 60
 
 
+def func4():
+    print ('func4')
+    freq = 10000
+    for i in range(10000000000):
+        sine_osc.play_frequencies(stream, .11, random.choice([.90, 1, 1, 1, 1.05]) / 2, 50, 50,
+                                  freq,
+                                  freq + 1,
+                                  )
+        freq += 50
 
 
 if __name__=='__main__':
@@ -65,4 +74,7 @@ if __name__=='__main__':
     p2.start()
     time.sleep(1)
     p3 = mp.Process(target=func3)
+    p3.start()
+    time.sleep(1)
+    p3 = mp.Process(target=func4)
     p3.start()
