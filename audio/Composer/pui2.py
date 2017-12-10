661d4c424fe6a182c40f7302392d5ca5aa3d7a63
Danny Meyer <danny.meyer@gmail.com>
2017-12-10 05:17:56 -0500

    working

--- audio/Composer/pui2.py
+++ audio/Composer/pui2.py
@@ -4,7 +4,7 @@ from clear_osc import SineOsc
 import multiprocessing as mp
 import time
 
-sine_osc = SineOsc()
+osc = SineOsc()
 
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32,
@@ -14,47 +14,127 @@ stream = p.open(format=pyaudio.paFloat32,
                 )
 
 
-Root = 120
+Root = 130
 
 def bassi():
     freq = Root
     print ('bass')
     length = 1.5
     for i in range(3):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq,
                                   )
 
     for i in range(3):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq * 9/8,
                                   )
 
     for i in range(3):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq * 5/4,
                                   )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 4/3,
                               )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 9/8,
                               )
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq,
                               )
 
     for i in range(3):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq * 3/2,
                                   )
 
     # ______________________________________
+
+
+    for i in range(3):
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
+                                  freq * 4/3,
+                                  )
+
+    for i in range(3):
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
+                                  freq * 5/4,
+                                  )
+
+    for i in range(3):
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
+                                  freq * 9/8,
+                                  )
+
+    for i in range(3):
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
+                                  freq,
+                                  )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 2,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 2 * 9/8,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 2 * 5/4,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 2 * 4/3,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 4/3,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 3/2,
+                              )
+
+    for i in range(3):
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
+                                  freq * 5/3,
+                                  )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 3/2,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 15/8,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 2,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 4/3,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 3/2,
+                              )
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 3/2 /2,
+                              )
+
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+                              freq,
+                              )
+
+
 
 def viola():
     freq = Root * 2
@@ -62,85 +142,85 @@ def viola():
     length = 1.5
 
     for i in range(3):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq * 5/4,
                                   )
 
     for i in range(2):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq * 9/8,
                                   )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 15/8 /2,
                               )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                               freq
                               )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq
                               )
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 9/8
                               )
 
-    sine_osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
                               freq * 5/4
                               )
 
-    sine_osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
                               freq * 3/2
                               )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                               freq * 3/2
                               )
 
 # ______________________________________
 
 def violino2():
-    freq = Root * 3
+    freq = Root * 2
     print ('violino2')
     length = 1.5
     for i in range(3):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq * 3/2,
                                   )
 
     for i in range(3):
-        sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+        osc.play_frequencies(stream, length, 1, 10000, 1000,
                                   freq * 4/3,
                                   )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                               freq * 5/4,
                               )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 5/3,
                               )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 4/3,
                               )
 
-    sine_osc.play_frequencies(stream, length / 2, 1, 10000, 1000,
+    osc.play_frequencies(stream, length / 2, 1, 10000, 1000,
                               freq * 3/2,
                               )
 
 
-    sine_osc.play_frequencies(stream, length / 2, 1, 10000, 1000,
+    osc.play_frequencies(stream, length / 2, 1, 10000, 1000,
                               freq * 2,
                               )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                                freq * 15/8,
                               )
 
@@ -150,65 +230,139 @@ def violino1():
     freq = Root * 4
     print ('violino1')
     length = 1.5
-    sine_osc.play_frequencies(stream, length * 4, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 4, 1, 10000, 1000,
                               freq,
                               )
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 15/8 /2,
                               )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * .75, 1, 10000, 1000,
                               freq * 5/3 /2,
                               )
 
-    sine_osc.play_frequencies(stream, length * .25, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * .25, 1, 10000, 1000,
                               freq * 3/2 /2,
                               )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                               freq * 3/2 /2,
                               )
 
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 5/3 /2,
                               )
 
-    sine_osc.play_frequencies(stream, length, 1, 10000, 1000,
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
                               freq * 15/8 /2,
                               )
 
-    sine_osc.play_frequencies(stream, length * .25, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * .25, 1, 10000, 1000,
                               freq
                               )
 
-    sine_osc.play_frequencies(stream, length * .25, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * .25, 1, 10000, 1000,
                               freq * 9/8
                               )
 
-    sine_osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
                               freq * 5/4
                               )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                               freq * 9/8
                               )
 
     # ______________________________________
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                               freq * 5/3
                               )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
                               freq * 3/2
                               )
 
-    sine_osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+    # time.sleep(1)
+
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+                              freq * 4/3
+                              )
+
+
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+                              freq * 5/4
+                              )
+
+    # time.sleep(1)
+
+    # _____________________________________
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 2
+                              )
+
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 2
+                              )
+
+
+    osc.play_frequencies(stream, length, 1, 10000, 1000,
+                              freq * 15/8
+                              )
+
+    osc.play_frequencies(stream, length * 1.5, 1, 10000, 1000,
+                              freq * 5/3
+                              )
+
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+                              freq * 3/2
+                              )
+
+    osc.play_frequencies(stream, length * 1.5, 1, 10000, 1000,
+                              freq * 3/2
+                              )
+
+    for i in range(2):
+        osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+                                  freq * 25/18
+                                  )
+        osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+                                  freq * 3/2
+                                  )
+
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+                              freq * 25/18
+                              )
+
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+                              freq * 3/2
+                              )
+
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+                              freq * 4/3
+                              )
+
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+                              freq * 5/4
+                              )
+
+    osc.play_frequencies(stream, length * 1.5, 1, 10000, 1000,
                               freq * 9/8
                               )
+
+    osc.play_frequencies(stream, length * .5, 1, 10000, 1000,
+                              freq
+                              )
+
+    osc.play_frequencies(stream, length * 3, 1, 10000, 1000,
+                              freq
+                              )
+
 
 if __name__=='__main__':
     mp.set_start_method('spawn')
@@ -216,7 +370,7 @@ if __name__=='__main__':
     p1.start()
     p2 = mp.Process(target=viola)
     p2.start()
-    # p3 = mp.Process(target=violino2())
-    # p3.start()
-    p4 = mp.Process(target=violino1())
+    p3 = mp.Process(target=violino2)
+    p3.start()
+    p4 = mp.Process(target=violino1)
     p4.start()
