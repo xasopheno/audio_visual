d161da82b27e65c0e49cb1df08d7c91933c234d1
Danny Meyer <danny.meyer@gmail.com>
2017-12-24 22:29:27 -0500

    WIP

--- audio/Multivariate/keras_multi.py
+++ audio/Multivariate/keras_multi.py
@@ -73,7 +73,7 @@ scaled = scaler.fit_transform(values)
 # frame as supervised learning
 
 # specify the number of lag hours
-n_hours = 3
+n_hours = 10
 n_features = 2
 # frame as supervised learning
 reframed = series_to_supervised(scaled, n_hours, 1)
@@ -98,44 +98,44 @@ test_X = test_X.reshape((test_X.shape[0], n_hours, n_features))
 print(train_X.shape, train_y_notes.shape, test_X.shape, test_y_notes.shape)
 
 visible = Input(name='input_main', shape=(train_X.shape[1], train_X.shape[2]))
-hidden1 = LSTM(256, return_sequences=True)(visible)
+hidden1 = LSTM(128, return_sequences=True)(visible)
 dropout1 = Dropout(0.5)(hidden1)
 
-hidden2 = LSTM(256, return_sequences=True)(dropout1)
+hidden2 = LSTM(128, return_sequences=True)(dropout1)
 dropout2 = Dropout(0.5)(hidden2)
 
-hidden3 = LSTM(256, return_sequences=True)(dropout2)
-dropout3 = Dropout(0.5)(hidden3)
+#hidden3 = LSTM(256, return_sequences=True)(dropout2)
+#dropout3 = Dropout(0.5)(hidden3)
+#
+#hidden4 = LSTM(256, return_sequences=True)(dropout3)
+#dropout4 = Dropout(0.5)(hidden4)
+#
+#hidden5 = LSTM(256, return_sequences=True)(dropout4)
+#dropout5 = Dropout(0.5)(hidden5)
+#
+#hidden6 = LSTM(256, return_sequences=True)(dropout5)
+#dropout6 = Dropout(0.5)(hidden6)
+#
+#hidden7 = LSTM(256, return_sequences=True)(dropout6)
+#dropout7 = Dropout(0.5)(hidden7)
+#
+#hidden8 = LSTM(256, return_sequences=True)(dropout7)
+#dropout8 = Dropout(0.5)(hidden8)
+#
+#hidden9 = LSTM(256, return_sequences=True)(dropout8)
+#dropout9 = Dropout(0.5)(hidden9)
+#
+#hidden10 = LSTM(256, return_sequences=True)(dropout9)
+#dropout10 = Dropout(0.5)(hidden10)
 
-hidden4 = LSTM(256, return_sequences=True)(dropout3)
-dropout4 = Dropout(0.5)(hidden4)
-
-hidden5 = LSTM(256, return_sequences=True)(dropout4)
-dropout5 = Dropout(0.5)(hidden5)
-
-hidden6 = LSTM(256, return_sequences=True)(dropout5)
-dropout6 = Dropout(0.5)(hidden6)
-
-hidden7 = LSTM(256, return_sequences=True)(dropout6)
-dropout7 = Dropout(0.5)(hidden7)
-
-hidden8 = LSTM(256, return_sequences=True)(dropout7)
-dropout8 = Dropout(0.5)(hidden8)
-
-hidden9 = LSTM(256, return_sequences=True)(dropout8)
-dropout9 = Dropout(0.5)(hidden9)
-
-hidden10 = LSTM(256, return_sequences=True)(dropout9)
-dropout10 = Dropout(0.5)(hidden10)
-
-hidden11 = LSTM(256)(dropout10)
+hidden11 = LSTM(128)(dropout2)
 
 output_notes = Dense(1, activation='sigmoid', name='output_notes')(hidden11)
 output_length = Dense(1, activation='sigmoid', name='output_length')(hidden11)
 
 model = Model(inputs=[visible], outputs=[output_notes, output_length])
 
-optimizer = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+optimizer = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 model.compile(loss='mae', optimizer=optimizer)
 model.summary()
 
@@ -145,7 +145,7 @@ history = model.fit(
             {'output_notes': train_y_notes, 'output_length': train_y_length},
             validation_data=({'input_main': test_X},
                              {'output_notes': test_y_notes, 'output_length': test_y_length}), 
-            verbose=1,
+            verbose=2,
             shuffle=False,
             epochs=50, batch_size=32)
 
