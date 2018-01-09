3f26ab450bc1de2c04df13e8995d84e2ec95e602
Danny Meyer <danny.meyer@gmail.com>
2018-01-09 13:00:24 -0500

    Refactored

--- audio/DataGeneration/StreamToMidi.py
+++ audio/DataGeneration/StreamToMidi.py
@@ -1,132 +1,16 @@
 import os.path
 import argparse
-import numpy
+
 import pyaudio
 import math
-import aubio
-import shutil
+
 import sys
 import time
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 current_path = os.getcwd()
 from Midi.MidiPlayer import MidiPlayer
-
-class StreamToFrequency:
-    def __init__(self, show_volume=False, store=None):
-        self.terminal_width = self.get_screen_width()
-        self.store = store
-        self.show_volume=show_volume
-        self.pDetection = aubio.pitch("yinfft", 2048, 2048, 44100)
-        self.pDetection.set_unit("midi")
-        self.pDetection.set_silence(-40)
-        self.pDetection.set_tolerance(.4)
-
-        self.output_file = open('Detection/output.txt', 'w')
-
-        self.volume_threshold = 6
-        self.acceptable_confidence = 1
-
-    def get_screen_width(self):
-        width = shutil.get_terminal_size((80, 20)).columns
-        return width
-
-    def callback(self, in_data, frame_count, time_info, status):
-        samples = numpy.fromstring(in_data,
-                                   dtype=aubio.float_type)
-
-        prediction = int(self.pDetection(samples)[0])
-
-        volume = numpy.sum(samples ** 2) / len(samples)
-
-        self.store.note = prediction
-        self.store.volume = volume
-
-        if self.show_volume:
-            self.__display_volume(volume)
-
-        return in_data, pyaudio.paContinue
-
-    def __display_volume(self, volume):
-        width = self.terminal_width
-        bar = ' ' + "-" * (int(volume * 100))
-        if len(bar) > width - 20:
-            bar = ' ' + "-" * (width - 20)
-        print(str(volume) + bar)
-
-    @staticmethod
-    def scale_velocity(volume):
-        volume = round(volume, 6) * 10 ** 3
-        volume = math.log(volume)
-        volume *= 15
-        if volume > 127:
-            volume = 127
-        if volume < 0:
-            volume = 0
-
-        return volume
-
-
-class Store:
-    def __init__(self):
-        self.__note = 0
-        self.__volume = 0
-        self.length = 1
-
-        self.volume_array = []
-
-    @property
-    def note(self):
-        return self.__note
-
-    @property
-    def volume(self):
-        return self.__volume
-
-    @note.setter
-    def note(self, note):
-        note = int(note)
-        self.__note = note
-
-    @volume.setter
-    def volume(self, volume):
-        volume = self.scale_volume(volume)
-        volume = int(volume)
-        self.volume_array.append(volume)
-        self.__volume = volume
-
-    @property
-    def values(self):
-        return {
-            "note": self.note,
-            "volume": self.avg_volume(),
-            "length": self.length,
-        }
-
-    def inc_length(self):
-        self.length += 1
-
-    @staticmethod
-    def scale_volume(volume):
-        volume = round(volume, 6) * 10 ** 3
-        volume = math.log(volume)
-        volume *= 15
-        if volume > 127:
-            volume = 127
-        if volume < 0:
-            volume = 0
-
-        return volume
-
-    def reset(self):
-        self.length = 1
-        self.volume_array = []
-
-    def avg_volume(self):
-        length = len(self.volume_array)
-        total = sum(self.volume_array)
-        avg = total/length if length else 0
-        return int(avg)
-
+from StreamToFrequency import StreamToFrequency
+from Store import Store
 
 class Generator:
     def __init__(self, args=None, store=None):
